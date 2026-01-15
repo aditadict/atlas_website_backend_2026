7daf522d7b6f91a5a6baeafc2d529c6c3d16f06b
Aditya Eka Arifyanto <adityaekaarifyanto@gmail.com>
2026-01-15 14:52:05 +0700

    fix: update access control logic in User model to allow all users

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,7 @@ class User extends Authenticatable
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@atlasdigitalize.com');
+        // return str_ends_with($this->email, '@atlasdigitalize.com');
+        return true;
     }
 }
